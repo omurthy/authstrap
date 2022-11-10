31cbcae10ed67a21a784251c944631096b85893b
omurthy <omuralcinthy@gmail.com>
2022-11-10 22:26:31 +0300

    adds p-icons and menubar elements

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { TokenStorageService } from './_services/token-storage.service';
- 
+
 
 @Component({
   selector: 'app-root',
@@ -14,9 +14,9 @@ export class AppComponent {
   showAdminBoard = false;
   showModeratorBoard = false;
   username?: string;
-  items : MenuItem[]=[];
+  items: MenuItem[] = [];
   constructor(private tokenStorageService: TokenStorageService) { }
-
+  
   ngOnInit(): void {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
 
@@ -29,44 +29,36 @@ export class AppComponent {
 
       this.username = user.username;
     }
-    this.items = [
+    this.items = [ 
       {
-          label:'File',
-          icon:'pi pi-fw pi-file',
-          items:[
-              {
-                  label:'New',
-                  icon:'pi pi-fw pi-plus',
-                  items:[
-                  {
-                      label:'Bookmark',
-                      icon:'pi pi-fw pi-bookmark'
-                  },
-                  {
-                      label:'Video',
-                      icon:'pi pi-fw pi-video'
-                  },
-
-                  ]
-              },
-              {
-                  label:'Delete',
-                  icon:'pi pi-fw pi-trash'
-              },
-              {
-                  separator:true
-              },
-              {
-                  label:'Export',
-                  icon:'pi pi-fw pi-external-link'
-              }
-          ]
-      } ,
+        label: 'Home',
+        icon: 'pi pi-fw pi-align-justify',
+        routerLink: ['/home'],
+      },
       {
-          label:'Quit',
-          icon:'pi pi-fw pi-power-off'
+        label: 'User',
+        icon: 'pi pi-fw pi-user',
+        routerLink: ['/user'],
+       },
+      {
+        label: 'Admin Board',
+        icon: 'pi pi-fw pi-calendar', 
+        routerLink: ['/admin'], 
+        visible: this.showAdminBoard 
       }
-  ];
+    ];
+  }
+
+  checkAdminBoard(){
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
   }
 
   logout(): void {
